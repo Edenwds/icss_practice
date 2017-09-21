e2156bf3591116dc3db9435c097dac5002aa8fd4
Edenwds <1466809510@qq.com>
2017-09-21 21:21:01 +0800

    动态生成新闻页面
    IO流模板替换

---
+++ NewsTest/src/com/icss/news/contants/TemplateParms.java
@@ -0,0 +1,9 @@
+package com.icss.news.contants;
+
+public class TemplateParms {
+
+	public static final String TITLE = "%{title}%";
+	public static final String CONTENT = "%{newsContent}%";
+	public static final String CATEGORY = "%{category}%";
+	public static final String PUB_TIME = "%{pubTime}%";
+}
---
+++ NewsTest/src/com/icss/news/entity/News.java
@@ -0,0 +1,36 @@
+package com.icss.news.entity;
+
+public class News {
+
+	private String title;
+	private String content;
+	private String category;
+	private String pubTime;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getPubTime() {
+		return pubTime;
+	}
+	public void setPubTime(String pubTime) {
+		this.pubTime = pubTime;
+	}
+	
+	
+}
