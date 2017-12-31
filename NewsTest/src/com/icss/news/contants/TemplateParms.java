8eb30d2e975f21dbb834a053d66533091eee1aca
Edenwds <1466809510@qq.com>
2017-12-31 13:54:02 +0800

    删除

--- NewsTest/src/com/icss/news/contants/TemplateParms.java
+++
@@ -1,9 +0,0 @@
-package com.icss.news.contants;
-
-public class TemplateParms {
-
-	public static final String TITLE = "%{title}%";
-	public static final String CONTENT = "%{newsContent}%";
-	public static final String CATEGORY = "%{category}%";
-	public static final String PUB_TIME = "%{pubTime}%";
-}
